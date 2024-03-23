fb20fbac4247f8935091b751f1d7bce4fe42aee8
shuai275 <275839408@qq.com>
2024-03-23 21:44:29 +0800

    add init files

---
+++ 20241-6月想去杭州旅遊，行程路線要怎樣安排？_files/12247.js
@@ -0,0 +1,18 @@
+/* 
+ * Make Time: 2024-03-23 15:31:04 
+ * Kefu Type: 新版本客服 
+ * 本客服系统由张家界鼎尖网络科技有限公司开发，已申请计算机软件著作权。
+ * 受《国家计算机软件著作权法》保护，禁止任何形式的抄袭盗用，否则我们将依法追究相关法律责任。
+ * 合作电话：0744-8383800 微信：18074477180 网址：www.zjjdj.com  法律顾问：代可律师
+ * 功能列表：
+ * 1、客服智能随机展示，一个页面可设置多个客服，访客访问会随机展现一个；
+ * 2、客服展现智能锁定：访客访问，展示A客服后，不管刷新，还是分享链接给微信好友，都是显示A客服；
+ * 3、手机，电脑，小程序，快速控制客服上下线；
+ * 4、客服24小时自动排班上下线；
+ * 5、客服可指定手机端和电脑端分开展现；
+ * 6、客服信息加密，防止客服资料泄露，加密内容如下：
+ */
+var $_STORAGENAME = '1702973058';
+var $_KFDEBUG = '0';
+var $_KFLOCK = '1';
+var $_KFINFO = '0zHSvILHAFvvV96So52zc/DxSzS49gcZRJJDRFJPFZ7E8bz5EaNQJuHC2Qcbbfl9lh75oASKxtHde0CSMI7A2eAotRblwHcCqN3ziB7ujQMThIMs1bPKnPgYtP4Z1m/Bw/DHmLeMVdZiqBmQ4O7RnuuuOKlqUXFawLDc3P6Ypmna84WnUioiTV2yR+zIF0HtO0KUhacGMbo8i1SwEiu+O2sPiBuYwQSrIzkqm/EsRXzlpSTP/6ycK/CIzEelprPEV7sUEdBgHEgvrZiFk2Wxi6BJYCdWCv3vw+5OBW4dGck5BZYSQm/jpiAVPf1G9esI3bZCWotsJbeqLSCV7/TzXDVbiefFUj5FOwWESLKzj+H5DGGVxH/xNw9zBhBpdW+p6giiIchL6YdHv1zidyuSJQ80xbD8fT7XF0eJpIaBI/9SKzd3W4udHmWRVvUSVp3g3XBbHG3rnxzND1bFOHvQ7iK0qmWzMNgEiP46L2kuLIR7p3D2B136yglNM9qGGSXNNYFxmR3MiMEXmgQPuPNN1BBAD1E9HpUCIIS4sVgeAYvvTABVOkXV9zA3ckKQIHfWnL3/sGRrQpevij5xa8BnFE6jYJca9JaB1MkTWL++GcsbuxXq34WY8WVxP/JyTw7Folg6aavWGXV3MeBNWVxGA0796vbFloxhIYB06Zl0E6JIVOkPzypVMCmJ9+LivG9F4xNtV97+AwzaJDoKyiSM6j8Jitwjddubm7If77Ms+i25RtV8vqa73pIlHNl2swPYkYi96a8+KHFDzCeJN5GJ2JQOhbayamhGwFyvhbpjL8XmDN3qJTJXvGufmjMqwcRlog/MIHynkSSIluFD2ODRCoFy7r4VcaotRHb88lNv0AttvtEw/mijo1+6Alf1V9d+8NCwSCmAQbjO5MqRXJWGSD+qPo9T0BAW3K06/vD6kMUKKXGTkPqcpSssul4ko8Nqj8RgRMULcLxsW6615r+EYgicUwflEXIhxcbG+lu4gvheuNCDevpOnFwPzhdOMnWYKz4qhqgnaWgdDYmmone3vdJ7skSMe0pyJbmZPrC70K1ucGGJAjFqaGpTCx6zi3bGLzO6vDHX606lrMAeLsRWZRdXWgeLr1sGDHEdXncxL8wq30KK1o+4/UpYklWBXtE6pqgAQB5EuU8gx4wHOuUsHpeLSfjO3cJ6uZIYV4ktwyG0MxVrIxdfkZrhbRPTjBHQFJ3hHEctA6MYlW3gvOi1dQ1vvH9WOFuHHT2aAJz8uALv3zFCbqK3+7GunTshGj2dHWmNdqbwFWlMcaCdvm7mQ3+2taJXDiL48xCI/ypNOa2fNZBKKpFYncShseKo52xIzok1Gk4cGiI0Kl8+mrNgsRcryEk7bV/5+ZuJHhRXsXVytYTsCfpewMil+bsmskskcon6HdHEOWulIP5q5fNJiXfnhjHl3q/ikKDsgSIgppXhcp2jPi9tvH7KE4hanzlxmwNoAsnuAgVYHSy0EcKWAx+kIwga4dORWSAQIaPFULWhjFJaDoc2wLMOWpjD8N9kNnhcM/RazFE9Ya3t6Em5U9figVYDayVBdco6nmBGj3ZGy92499CCQfYa2CxEHlMyzhAA5Sslnffsrz+l3fLhqbHzgWQieAqu3BQGZC1P8zYqyvdomZRPE+btTB4DkVU5nvn4jj9WKHUOUy8OOb/bb/8MU/Mnh8ZmZMgXPPTuxbw=';
