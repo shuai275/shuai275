a698f04cb5b47b63243be5725e64ae34a31062b1
shuai275 <275839408@qq.com>
2024-03-25 22:09:44 +0800

    remove js

--- src/wsslide.js
+++
@@ -1,9 +0,0 @@
-itzjj.use([ "swiper"], function () {
-  var swiper = itzjj.swiper;
-  var mySwiper = new swiper(".swiper", {
-    autoplay: true,
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
-});
