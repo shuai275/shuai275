1c13c63f1129addd1306243c1483da14a72b7785
shuai275 <275839408@qq.com>
2024-03-29 01:11:21 +0800

    init file

---
+++ trip2BeiJing6_files/wsslide.js
@@ -0,0 +1,9 @@
+itzjj.use([ "swiper"], function () {
+  var swiper = itzjj.swiper;
+  var mySwiper = new swiper(".swiper", {
+    autoplay: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+});
